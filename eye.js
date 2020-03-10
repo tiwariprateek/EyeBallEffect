67e36a3e6bbd5d5c32541887991921f66c7e7f8b
tiwariprateek <prateektiwari5399@gmail.com>
2020-03-10 12:55:20 +0530

    Eye ball effect

---
+++ eye.js
@@ -0,0 +1,7 @@
+document.onmousemove=function(){
+	let x=event.clientX*100/window.innerWidth+"%";
+	let y=event.clientY*100/window.innerHeight+"%";
+	eye_ball=document.getElementById("eye-ball")
+	eye_ball.style.left=x;
+	eye_ball.style.top=y;
+}
